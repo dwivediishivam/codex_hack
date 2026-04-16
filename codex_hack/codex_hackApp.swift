366e2350964717cf54ebfac15f74a515e6c6a41c
Shivam Dwivedi <shivam21352@iiitd.ac.in>
2026-04-16 13:01:09 +0530

    Initial Commit

---
+++ codex_hack/codex_hackApp.swift
@@ -0,0 +1,17 @@
+//
+//  codex_hackApp.swift
+//  codex_hack
+//
+//  Created by Shivam Dwivedi on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct codex_hackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
